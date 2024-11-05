99cf6ab7429d18640d650e6edb901564d54ad232
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-05 21:02:07 +0700

    update UI userInfor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React, { useEffect } from 'react';
-import { useDispatch} from 'react-redux';
+
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { get_category } from './store/reducers/homeReducer';
+import { useDispatch} from 'react-redux';
+
+import Dashboard from './pages/Dashboard';
+import ProtectUser from './utils/ProtectUser';
+import Index from './components/dashboard/Index';
+import Orders from './components/dashboard/Orders';
+import ChangePassword from './components/dashboard/ChangePassword';
+import Wishlist from './components/dashboard/Wishlist';
+import OrderDetails from './components/dashboard/OrderDetails';
 
 
 
-function App (){
+
+function App() {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(get_category()) 
 },[])
+
 
   return (
     <BrowserRouter>
@@ -21,6 +34,23 @@ function App (){
       <Route path='/' element={<Home/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
+      <Route path='/shops' element={<Shops/>} />
+
+
+      <Route path='/dashboard' element={<ProtectUser/>} >
+      <Route path='' element={<Dashboard/>} >        
+      <Route path='' element={<Index/>} />
+      <Route path='my-orders' element={<Orders/>} /> 
+      <Route path='change-password' element={<ChangePassword/>} /> 
+      <Route path='my-wishlist' element={<Wishlist/>} /> 
+      <Route path='order/details/:orderId' element={<OrderDetails/>} /> 
+
+       
+       </Route> 
+      </Route>
+      
+       
+
 
 
     </Routes>
@@ -30,3 +60,4 @@ function App (){
 }
 
 export default App;
+;
---
+++ src/pages/Shops.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shops = () => {
+    return (
+        <div>
+            shop
+        </div>
+    );
+};
+
+export default Shops;
