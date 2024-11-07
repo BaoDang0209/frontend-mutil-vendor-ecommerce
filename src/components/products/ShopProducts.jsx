225635526f23c5f585028909cb9ac68a1b49b169
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2024-11-07 21:01:25 +0700

    update shopproducts

--- src/components/products/ShopProducts.jsx
+++ src/components/products/ShopProducts.jsx
@@ -6,7 +6,7 @@ import Rating from '../Rating';
 const ShopProducts = ({styles,products}) => {
     return (
         <div className={`w-full grid ${styles === 'grid' ? 'grid-cols-3 md-lg:grid-cols-2 md:grid-cols-2' : 'grid-cols-1 md-lg:grid-cols-2 md:grid-cols-2'} gap-3 `}>
-            {
+            {   
                 products.map((p, i)=> <div key={i} className={`flex transition-all duration-1000 hover:shadow-md hover:-translate-y-3 ${styles === 'grid' ? 'flex-col justify-start items-start' : 'justify-start items-center md-lg:flex-col md-lg:justify-start md-lg:items-start'} w-full gap-4 bg-white p-1 rounded-md`}>
 
         <div className={styles === 'grid' ? 'w-full relative group h-[210px] md:h-[270px] xs:h-[170px] overflow-hidden' : 'md-lg:w-full relative group h-[210px] md:h-[270px] overflow-hidden'}>
